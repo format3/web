e41fee84fa09e3717c18d4580b17a742c83725ec
mbf <PrincessMatty@Gennas-MacBook-Pro.local>
2017-11-30 13:11:27 -0500

    favicon add

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
